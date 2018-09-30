6dd39c99378eec9a91a191332fb6693e937cbb6b
Charpell <charpellumeh@gmail.com>
2018-09-30 13:56:06 +0100

    Customer API completed

--- genres/index.js
+++ genres/index.js
@@ -1,4 +1,5 @@
 const genres = require('./routes/genres');
+const customers = require('./routes/customers');
 const express = require('express');
 const app = express();
 
@@ -8,6 +9,7 @@ mongoose.connect('mongodb://localhost/vidly')
 
 app.use(express.json());
 app.use('/api/genres', genres);
+app.use('/api/customers', customers);
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on port ${port}...`));
